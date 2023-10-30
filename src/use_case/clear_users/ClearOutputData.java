162549b01e05ed1a1331d9bae00fce749b8b34aa
Kevin <q.hu@mail.utoronto.ca>
2023-10-30 16:56:58 -0400

    remove template files to prepare for merge

--- src/use_case/clear_users/ClearOutputData.java
+++
@@ -1,9 +0,0 @@
-package use_case.clear_users;
-
-public class ClearOutputData {
-    public final String[] usernames;
-
-    public ClearOutputData(String[] usernames) {
-        this.usernames = usernames;
-    }
-}
