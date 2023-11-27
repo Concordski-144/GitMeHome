833fc4c226e62828d2e89cc82fe563dccf5dc751
Joseph <meizhou.su@mail.utoronto.ca>
2023-11-27 11:38:36 -0500

    Edited the controller

--- src/interface_adapter/get_details/GetDetailsController.java
+++ src/interface_adapter/get_details/GetDetailsController.java
@@ -1,4 +1,13 @@
 package interface_adapter.get_details;
 
+import use_case.get_details.GetDetailsInputData;
+import use_case.next_departures.NextDeparturesInputData;
+
 public class GetDetailsController {
+    public void execute(String stationID) {
+        GetDetailsInputData getDetailsInputData = new GetDetailsInputData(
+                stationID);
+
+        getDetailsInteractor.execute(getDetailsInputData);
+    }
 }
