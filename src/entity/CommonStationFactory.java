7fe23cb369258d8196582a25dd8dbb05030252c1
Franklin <franklin.wu@mail.utoronto.ca>
2023-11-28 22:54:53 -0500

    renamed and made Station.getTransitMode() return an integer according to GTFS standard since one stop can only serve one transit mode

--- src/entity/CommonStationFactory.java
+++ src/entity/CommonStationFactory.java
@@ -2,7 +2,7 @@ package entity;
 
 public class CommonStationFactory implements StationFactory {
     @Override
-    public Station create(String name, String id, double longitude, double latitude, boolean accessibility, Integer transitMode, int distance_from_user, Route[] routes){
+    public Station create(String name, String id, double longitude, double latitude, boolean accessibility, int transitMode, int distance_from_user, Route[] routes){
         return new CommonStation(name, id, longitude, latitude, accessibility, transitMode, distance_from_user, routes);
     }
 }
