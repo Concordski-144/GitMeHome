a35dc441cf2328755ebb3b439b779ad67f823967
Kevin <q.hu@mail.utoronto.ca>
2023-12-04 15:38:19 -0500

    Modified Interactor to add the ability to check delay by station, split the output of checking by route into another class

--- src/use_case/check_delays/CheckDelaysInteractor.java
+++ src/use_case/check_delays/CheckDelaysInteractor.java
@@ -9,25 +9,34 @@ import java.util.HashMap;
 import java.util.List;
 
 public class CheckDelaysInteractor implements CheckDelaysInputBoundary {
-    final CheckDelaysDataAccessInterface nextDepartureDataAccessObject;
-    final CheckDelaysOutputBoundary nextDeparturePresenter;
+    final CheckDelaysDataAccessInterface checkDelaysDataAccessObject;
+    final CheckDelaysOutputBoundary checkDelaysPresenter;
 
-    public CheckDelaysInteractor(CheckDelaysDataAccessInterface nextDepartureDataAccessObject, CheckDelaysOutputBoundary checkDelaysPresenter) {
-        this.nextDepartureDataAccessObject = nextDepartureDataAccessObject;
-        this.nextDeparturePresenter = checkDelaysPresenter;
+    public CheckDelaysInteractor(CheckDelaysDataAccessInterface checkDelaysDataAccessObject, CheckDelaysOutputBoundary checkDelaysPresenter) {
+        this.checkDelaysDataAccessObject = checkDelaysDataAccessObject;
+        this.checkDelaysPresenter = checkDelaysPresenter;
     }
 
 
     @Override
     public void execute(CheckDelaysInputData checkDelaysInputData) {
-        ArrayList<Delay> delays = nextDepartureDataAccessObject.checkDelaysByRoute(checkDelaysInputData.getId());
-        CheckDelaysOutputData nextDepartureOutputData = null;
-        HashMap<String, String> delayStatus = null;
-        for (Delay delay : delays) {
-            String routeDelayStatus = delay.getDelayStatus();
-            delayStatus.put(delay.getid(), routeDelayStatus);
+        //temporarily disabled due to bug in DAO
+//        ArrayList<Delay> delays = checkDelaysDataAccessObject.checkDelaysByRoute(checkDelaysInputData.getId());
+
+//        CheckDelaysOutputData checkDelaysOutputData = null;
+//        HashMap<String, String> delayStatus = null;
+//        for (Delay delay : delays) {
+//            String routeDelayStatus = delay.getDelayStatus();
+//            delayStatus.put(delay.getid(), routeDelayStatus);
+//        }
+        try {
+            boolean delayStatus = checkDelaysDataAccessObject.checkDelaysByStation(checkDelaysInputData.getId());
+            CheckDelaysOutputData checkDelaysOutputData = new CheckDelaysOutputData(delayStatus, false);
+            checkDelaysPresenter.prepareSuccessView(checkDelaysOutputData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            checkDelaysPresenter.prepareFailView("Error: " + e.getMessage());
         }
-        nextDepartureOutputData = new CheckDelaysOutputData(delayStatus, false);
-        nextDeparturePresenter.prepareSuccessView(nextDepartureOutputData);
+
     }
 }
--- src/use_case/check_delays/CheckDelaysOutputData.java
+++ src/use_case/check_delays/CheckDelaysOutputData.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CheckDelaysOutputData {
-    private final HashMap<String, String> delaysByRoute;
+    private final boolean status;
     private boolean useCaseFailed;
 
-    public CheckDelaysOutputData(HashMap<String,  String> delaysByRoute, boolean useCaseFailed) {
-        this.delaysByRoute = delaysByRoute;
+    public CheckDelaysOutputData(boolean status, boolean useCaseFailed) {
+        this.status = status;
         this.useCaseFailed = useCaseFailed;
     }
 
-    public HashMap<String,  String> getDelaysByRoute() {
-        return delaysByRoute;
+    public boolean getDelayStatus() {
+        return status;
     }
 }
