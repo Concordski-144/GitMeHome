f343b49025b5c0de27a17a2a797bb9eb8a82e5ed
Kevin <q.hu@mail.utoronto.ca>
2023-12-04 14:35:15 -0500

    Added CheckDelaysUseCaseFactory and modified Main to include view of CheckDelays

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,16 @@
 package app;
 
+import data_access.CheckDelaysDataAccessObject;
 import data_access.NextDeparturesDataAccessObject;
 import data_access.PlanATripDataAccessObject;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.next_departures.NextDeparturesViewModel;
 import interface_adapter.plan_a_trip.PlanATripViewModel;
+import interface_adapter.check_delays.CheckDelaysViewModel;
 import use_case.next_departures.NextDeparturesDataAccessInterface;
 import use_case.plan_a_trip.PlanATripDataAccessInterface;
+import use_case.check_delays.CheckDelaysDataAccessInterface;
+import view.CheckDelaysView;
 import view.NextDeparturesView;
 import view.PlanATripView;
 import view.ViewManager;
@@ -31,15 +35,20 @@ public class Main {
 
         NextDeparturesViewModel nextDepartureViewModel = new NextDeparturesViewModel();
         PlanATripViewModel planATripViewModel = new PlanATripViewModel();
+        CheckDelaysViewModel checkDelaysViewModel = new CheckDelaysViewModel();
 
         NextDeparturesDataAccessInterface nextDepartureDataAccessInterface = new NextDeparturesDataAccessObject();
         PlanATripDataAccessInterface planATripDataAccessInterface = new PlanATripDataAccessObject();
+        CheckDelaysDataAccessInterface checkDelaysDataAccessInterface = new CheckDelaysDataAccessObject();
 
         NextDeparturesView nextDepartureView = NextDeparturesUseCaseFactory.create(viewManagerModel, nextDepartureViewModel, nextDepartureDataAccessInterface);
         views.add(nextDepartureView, nextDepartureView.viewName);
 
         PlanATripView planATripView = PlanATripUseCaseFactory.create(viewManagerModel, planATripViewModel, planATripDataAccessInterface);
         views.add(planATripView, planATripView.viewName);
+
+        CheckDelaysView checkDelaysView = CheckDelaysUseCaseFactory.create(viewManagerModel, checkDelaysViewModel, checkDelaysDataAccessInterface);
+        views.add(checkDelaysView, checkDelaysView.viewName);
 
         viewManagerModel.setActiveView(planATripView.viewName);
         viewManagerModel.firePropertyChanged();
